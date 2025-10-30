d0913df395bbf3cea00efac9d23240066106c468
Paresh Karnawat <paresh.karnawat@gmail.com>
2025-10-30 09:58:10 +0530

    Initial Commit

---
+++ SwiftChartsDemo/SwiftChartsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsDemoApp.swift
+//  SwiftChartsDemo
+//
+//  Created by Paresh  Karnawat on 30/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
